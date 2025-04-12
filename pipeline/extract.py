26a4631956dcc7e2e92fc28c537a2db61c1dad93
GP <gowthampentela@gmail.com>
2025-04-11 20:55:34 -0500

    Modeified Extract.py

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,7 +1,11 @@
+import os
 import requests
 
 def download_trip_data(url, output_path):
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
     r = requests.get(url, stream=True)
     with open(output_path, "wb") as f:
-        for chunk in r.iter_content(chunk_size=1024*1024):
+        for chunk in r.iter_content(chunk_size=1024 * 1024):
             f.write(chunk)
+    print(f"Downloaded data from {url} to {output_path}")
